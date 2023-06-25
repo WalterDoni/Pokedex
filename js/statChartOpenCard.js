5d82c3e7a9e3531daa15257e06cb1104c9c906cc
Walter Doni <walter.doni@gmx.at>
2023-06-25 18:30:33 +0200

    change color

--- js/statChartOpenCard.js
+++ js/statChartOpenCard.js
@@ -16,7 +16,14 @@ async function bars(id) {
 
             datasets: [{
                 label: 'Basestats',
-                backgroundColor: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                backgroundColor: [
+                  'rgb(255, 65, 27)',
+                  'rgb(98, 189, 90',
+                  'rgb(43, 155, 227)',
+                  'rgb(187, 85, 68)', 
+                  'rgb(110, 67, 112)', 
+                  'rgb(86, 112, 190)'
+                ],
                 data: [clickedPokemon['stats']['0']['base_stat'], clickedPokemon['stats']['1']['base_stat'], clickedPokemon['stats']['2']['base_stat'], clickedPokemon['stats']['3']['base_stat'], clickedPokemon['stats']['4']['base_stat'], clickedPokemon['stats']['5']['base_stat']],
                 borderColor: [
                     'white',
